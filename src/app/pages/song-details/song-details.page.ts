f1b3f4ffeb53296051ec859c794b3bfd6da4fe2d
chahihos <chahihos@sct-22-2fd3l.ur.univ-rouen.fr>
2025-10-24 09:47:47 +0200

    last push probably

--- src/app/pages/song-details/song-details.page.ts
+++ src/app/pages/song-details/song-details.page.ts
@@ -20,32 +20,96 @@ export class SongDetailsPage implements OnInit {
   songTitle: string | null = null;
   song: Song | undefined;
 
-  songs: Song[] = [
-    {
-      title: 'Wish You Were Here',
-      artist: 'Pink Floyd',
-      duration: '5:30',
-      album: 'Wish You Were Here',
-      albumArt: 'https://example.com/pink-floyd.jpg',
-      selected: false,
-    },
-    {
-      title: 'Gimme Shelter',
-      artist: 'The Rolling Stones',
-      duration: '4:31',
-      album: 'Let It Bleed',
-      albumArt: 'https://example.com/rolling-stones.jpg',
-      selected: false,
-    },
-    {
-      title: 'Come Together',
-      artist: 'The Beatles',
-      duration: '4:19',
-      album: 'Abbey Road',
-      albumArt: 'https://example.com/beatles.jpg',
-      selected: false,
-    },
-  ];
+songs: Song[] = [
+  {
+    title: "There's a Light That Never Goes Out",
+    artist: 'The Smiths',
+    duration: '4:02',
+    album: 'The Queen Is Dead',
+    albumArt: 'assets/albums/queen_is_dead.png',
+    selected: false,
+  },
+  {
+    title: 'No Surprises',
+    artist: 'Radiohead',
+    duration: '3:49',
+    album: 'OK Computer',
+    albumArt: 'assets/albums/OK_computer.png',
+    selected: false,
+  },
+  {
+    title: 'Little Wing',
+    artist: 'Jimi Hendrix',
+    duration: '2:25',
+    album: 'Axis: Bold as Love',
+    albumArt: 'assets/albums/hendrix.png', // placeholder if missing
+    selected: false,
+  },
+  {
+    title: 'About a Girl',
+    artist: 'Nirvana',
+    duration: '2:48',
+    album: 'Bleach',
+    albumArt: 'assets/albums/bleach.png', // adjust if you download bleach later
+    selected: false,
+  },
+  {
+    title: 'Twist and Shout',
+    artist: 'The Beatles',
+    duration: '2:36',
+    album: 'Please Please Me',
+    albumArt: 'assets/albums/Please_please_me.png',
+    selected: false,
+  },
+  {
+    title: 'Should Have Known Better',
+    artist: 'Sufjan Stevens',
+    duration: '5:07',
+    album: 'Carrie & Lowell',
+    albumArt: 'assets/albums/Carryandlowel.png',
+    selected: false,
+  },
+  {
+    title: 'Stain',
+    artist: 'Nirvana',
+    duration: '2:38',
+    album: 'Incesticide',
+    albumArt: 'assets/albums/insesticide.png',
+    selected: false,
+  },
+  {
+    title: 'Orion',
+    artist: 'Metallica',
+    duration: '8:27',
+    album: 'Master of Puppets',
+    albumArt: 'assets/albums/master-of-puppets.jpg',
+    selected: false,
+  },
+  {
+    title: 'Wish You Were Here',
+    artist: 'Pink Floyd',
+    duration: '5:30',
+    album: 'Wish You Were Here',
+    albumArt: 'assets/albums/pinkfloyd.png',
+    selected: false,
+  },
+  {
+    title: 'Gimme Shelter',
+    artist: 'The Rolling Stones',
+    duration: '4:31',
+    album: 'Let It Bleed',
+    albumArt: 'assets/albums/letitbleed.png',
+    selected: false,
+  },
+  {
+    title: 'Come Together',
+    artist: 'The Beatles',
+    duration: '4:19',
+    album: 'Abbey Road',
+    albumArt: 'assets/albums/abbey-road.jpg',
+    selected: false,
+  },
+];
 
   constructor(private route: ActivatedRoute) {}
 
